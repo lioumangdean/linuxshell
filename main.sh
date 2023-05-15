ee80b48a97cd7db1c8ef63c516d961a38ae32519
lioumangdean <44220609+lioumangdean@users.noreply.github.com>
2023-05-15 13:26:34 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-sudo apt install net tool
+sudo apt install net-tools 
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt update && sudo apt upgrade -y
 sudo iptables -P INPUT ACCEPT
@@ -8,3 +8,9 @@ sudo iptables -F
 sudo apt autoremove netfilter-persistent* -y
 sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+sudo apt install speedtest-cli
+speedtest
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+speedtest
