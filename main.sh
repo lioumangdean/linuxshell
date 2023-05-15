fffba5db62ba5dd9128775a59dcd0bba945c16f9
lioumangdean <44220609+lioumangdean@users.noreply.github.com>
2023-05-15 10:34:59 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,7 @@
 sudo apt-get update
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -F
+sudo apt autoremove netfilter-persistent* -y
