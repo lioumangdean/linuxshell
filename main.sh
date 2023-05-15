8eb640952defc6b7c78b098178fccb9af0cb8ad1
lioumangdean <44220609+lioumangdean@users.noreply.github.com>
2023-05-15 10:40:22 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,9 @@
-sudo apt-get update
+sudo apt-get update && sudo apt-get upgrade -y
 sudo apt update && sudo apt upgrade -y
 sudo iptables -P INPUT ACCEPT
 sudo iptables -P OUTPUT ACCEPT
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -F
 sudo apt autoremove netfilter-persistent* -y
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
